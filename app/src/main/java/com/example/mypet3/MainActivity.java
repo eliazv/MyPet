5985aa8641d5243375cced193502e2477fd65670
Eliazv <zavattaelia@gmail.com>
2022-06-15 18:35:27 +0200

    Aggiunta nav bar, tutto da rifare

--- app/src/main/java/com/example/mypet3/MainActivity.java
+++ app/src/main/java/com/example/mypet3/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mypet3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +13,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import com.example.mypet3.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());//R.layout.activity_main);
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener( item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.map:
+                    replaceFragment(new MapFragment());
+                    break;
+                case R.id.camera:
+                    replaceFragment(new CamFragment());
+                    break;
+            }
+
+            return true;
+        });
 
 
         Button buttMap = (Button) findViewById(R.id.btnMap);
@@ -46,5 +72,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(CamInt);
             }
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
