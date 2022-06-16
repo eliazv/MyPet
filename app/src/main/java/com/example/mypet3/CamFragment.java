c542b66ab2f9f8b8b618014704e3aec0d809f9dd
Eliazv <zavattaelia@gmail.com>
2022-06-16 16:55:13 +0200

    aggiunta Camera

--- app/src/main/java/com/example/mypet3/CamFragment.java
+++ app/src/main/java/com/example/mypet3/CamFragment.java
@@ -1,64 +1,63 @@
 package com.example.mypet3;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CamFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+
 public class CamFragment extends Fragment {
+        private CodeScanner mCodeScanner;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            final Activity activity = getActivity();
+            View root = inflater.inflate(R.layout.fragment_cam, container, false);
+            CodeScannerView scannerView = root.findViewById(R.id.scanner_view);
+            mCodeScanner = new CodeScanner(activity, scannerView);
+            mCodeScanner.setDecodeCallback(new DecodeCallback() {
+                @Override
+                public void onDecoded(@NonNull final Result result) {
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(activity, result.getText(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            });
+            scannerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mCodeScanner.startPreview();
+                }
+            });
+            return root;
+        }
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+        @Override
+        public void onResume() {
+            super.onResume();
+            mCodeScanner.startPreview();
+        }
 
-    public CamFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CamFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CamFragment newInstance(String param1, String param2) {
-        CamFragment fragment = new CamFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        @Override
+        public void onPause() {
+            mCodeScanner.releaseResources();
+            super.onPause();
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cam, container, false);
-    }
-}
