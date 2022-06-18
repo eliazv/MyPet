130fdde93bf37d99d08d54752915821ed401e323
Eliazv <zavattaelia@gmail.com>
2022-06-18 11:41:22 +0200

    Get data from DB

--- app/src/main/java/DBClass/Pet.java
+++ app/src/main/java/DBClass/Pet.java
@@ -5,14 +5,31 @@ public class Pet {
     public String descr;
     public String posiz;
     public String specie;
+    public String email;
+    //immagine
+    //qr
 
     Pet(){}
+    //senza user email
     public Pet(String nome, String descr, String posiz, String specie){
         this.nome=nome;
         this.descr=descr;
         this.posiz=posiz;
         this.specie=specie;
     }
+
+
+    public Pet(String nome, String descr, String posiz, String specie, String email){
+        this.nome=nome;
+        this.descr=descr;
+        this.posiz=posiz;
+        this.specie=specie;
+        this.email=email;
+    }
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
 
     public String getNome() {
         return nome;
@@ -30,9 +47,7 @@ public class Pet {
         this.descr = descr;
     }
 
-    public String getSpecie() {
-        return specie;
-    }
+    public String getSpecie() {return specie;}
 
     public void setSpecie(String specie) {
         this.specie = specie;
--- app/src/main/java/com/example/mypet3/HomeFragment.java
+++ app/src/main/java/com/example/mypet3/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.mypet3;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +13,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import DBClass.Pet;
 
 public class HomeFragment extends Fragment {
 
@@ -23,6 +36,29 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //da riempire
+        ArrayList<Pet> listaPet = new ArrayList<>();
+
+        Query getPetData = FirebaseDatabase.getInstance().getReference("Pet");
+        Query getPetData2 = FirebaseDatabase.getInstance().getReference().child("Pet");
+        Query getPetName = getPetData.orderByChild("Nome");
+
+        getPetName.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot sn : snapshot.getChildren()){
+                    Pet p = sn.getValue(Pet.class);
+                    //aggiungi a lista?
+                    listaPet.add(p);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
     }
@@ -48,6 +84,10 @@ public class HomeFragment extends Fragment {
                 startActivity(AddInt);
             }
         });
+
+        //TextView PetName = (TextView) getView().findViewById(R.id.info_text);
+        //PetName.setText("a");
+
         return view;
     }
 
