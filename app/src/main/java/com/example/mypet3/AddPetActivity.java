e42a01f22489ab6b8d692e697c2644a618f891a7
Eliazv <zavattaelia@gmail.com>
2022-06-27 15:24:59 +0200

    bottone aggiunta immagini in add pet

--- app/src/main/java/com/example/mypet3/AddPetActivity.java
+++ app/src/main/java/com/example/mypet3/AddPetActivity.java
@@ -3,21 +3,36 @@ package com.example.mypet3;
 import static com.example.mypet3.LoginActivity.loggedUser;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mypet3.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import DBClass.DBPet;
 import DBClass.Pet;
@@ -25,7 +40,12 @@ import DBClass.Pet;
 public class AddPetActivity extends AppCompatActivity {
 
     DatabaseReference dbRef;
-    DBPet pushPetInDB;
+
+    ActivityMainBinding binding;
+    Uri imageUri;
+    StorageReference storageReference;
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +62,18 @@ public class AddPetActivity extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+
+        Button buttImg =  findViewById(R.id.btnAddPetImage);
+        buttImg.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //selectImage();
+
+                //uploadImage();//non qui ma in aggiungi
+            }
+        });
+
 
         Button buttAdd = findViewById(R.id.btnAddPet);
         buttAdd.setOnClickListener(view -> addPet());
@@ -108,4 +140,62 @@ public class AddPetActivity extends AppCompatActivity {
             });
         }
     }
+/*
+    public void selectImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,100);
+    }
+
+    private void uploadImage() {
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading File....");
+        progressDialog.show();
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.ITALY);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+
+        //textview
+        //TextView PetNameD =  findViewById(R.id.tvImageName);
+        //PetNameD.setText(fileName);
+
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        //binding.firebaseimage.setImageURI(null);
+                        //binding.imageView4.setImageURI(null);
+                        Toast.makeText(getApplicationContext(),"Successfully Uploaded",Toast.LENGTH_SHORT).show();
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (progressDialog.isShowing())
+                            progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(),"Failed to Upload",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null){
+
+            imageUri = data.getData();
+            //binding.imageView4.setImageURI(imageUri);
+
+        }
+    }*/
 }
