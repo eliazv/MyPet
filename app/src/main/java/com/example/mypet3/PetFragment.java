d3ff23799296a1a2f332eb53d3139f0db1376159
Eliazv <zavattaelia@gmail.com>
2022-06-29 12:27:22 +0200

    Pet specifico backup

--- app/src/main/java/com/example/mypet3/PetFragment.java
+++ app/src/main/java/com/example/mypet3/PetFragment.java
@@ -1,11 +1,14 @@
 package com.example.mypet3;
 
+import static com.example.mypet3.LoginActivity.loggedUser;
+
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,7 +17,13 @@ import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.zxing.BarcodeFormat;
@@ -30,12 +39,15 @@ import androidmads.library.qrgenearator.QRGEncoder;
 
 public class PetFragment extends Fragment {
 
-    public QRGEncoder qrgEncoder;
-    ImageView qrIV, petImg;
+
     StorageReference storageReference;
     FirebaseStorage storage;
+    DatabaseReference dbRef;
+
+    ImageView qrIV, petImg;
+
     String idPet;
-    String nomePet, user;
+    String nomePet, user="e";
 
     public PetFragment(){
 
@@ -64,12 +76,57 @@ public class PetFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_pet, container, false);
 
 
-        //---Set Data
+        //-----Set Data
+        dbRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://mypet---android-app-default-rtdb.firebaseio.com/");
+        dbRef.child("Pet").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild(idPet+" - e")) {//TODO metti user al posto di e
+                    String getDescr = snapshot.child(idPet+" - e").child("descrizione").getValue(String.class);
+                    TextView descr =  view.findViewById(R.id.tvDescrPetFr);
+                    descr.setText(getDescr);
 
-        //prendi dal db
-        
+                    String getCasa = snapshot.child(idPet+" - e").child("indirizzo").getValue(String.class);
+                    TextView casa =  view.findViewById(R.id.tvCasaPetFr);
+                    casa.setText(getCasa);
+
+                    String getImg = snapshot.child(idPet+" - e").child("img").getValue(String.class);
+                    //TODO prendere da storage e settare img
+                    
+                } else {
+                    Toast.makeText(getContext(), "Utente non trovato.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        dbRef.child("User").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.hasChild(user)) {
+                    String getTel = snapshot.child(user).child("telefono").getValue(String.class);
+                    TextView telefono =  view.findViewById(R.id.tvTelPetFr);
+                    telefono.setText(getTel);
+                } else {
+                    Toast.makeText(getContext(), "Utente non trovato.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         TextView PetNameD =  view.findViewById(R.id.tvNamePetFr);
         PetNameD.setText(idPet);
+
+        TextView tvUser =  view.findViewById(R.id.tvPadronePetFr);
+        tvUser.setText(user);
 
 
 
